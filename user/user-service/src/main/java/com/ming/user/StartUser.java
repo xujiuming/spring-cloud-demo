06d33d418ce97754302455dcdf8a50e1e9379cf2
ming <xujiuming@mucang.cn>
2021-12-05 14:51:41 +0800

    chore:增加mq部署

--- user/user-service/src/main/java/com/ming/user/StartUser.java
+++ user/user-service/src/main/java/com/ming/user/StartUser.java
@@ -1,12 +1,15 @@
 package com.ming.user;
 
 
+import com.ming.user.api.MqService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(MqService.class)
 public class StartUser {
     public static void main(String[] args) {
         SpringApplication.run(StartUser.class, args);
