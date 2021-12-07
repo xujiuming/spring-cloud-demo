e0ef7f1875584e64f6a1552cb22ba4508e146594
许久明 <xujiuming@mucang.cn>
2021-12-07 14:32:45 +0800

    feat:增加rsocket

--- rsocket/src/test/java/com/ming/rsocket/TestClient.java
+++ rsocket/src/test/java/com/ming/rsocket/TestClient.java
@@ -1,11 +1,29 @@
 package com.ming.rsocket;
 
+import io.rsocket.Payload;
 import io.rsocket.RSocket;
-import io.rsocket.core.RSocketClient;
+import io.rsocket.core.RSocketConnector;
+import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
+import reactor.core.publisher.Flux;
 
 public class TestClient {
     public static void main(String[] args) {
-        RSocketClient client = RSocketClient.from(TcpClientTransport.create(20000));
+        RSocket clientRSocket =
+                RSocketConnector.create()
+                        // Enable Zero Copy
+                        .payloadDecoder(PayloadDecoder.ZERO_COPY)
+                        .connect(TcpClientTransport.create(20000))
+                        .block();
+//        clientRSocket.requestResponse(DefaultPayload.create("test-rsocket-req-res"))
+//                .subscribe(s-> System.out.println(s.data().toString()));
+
+        try {
+            Flux<Payload> s = clientRSocket.requestStream(DefaultPayload.create("ffffffffffff", "test-rsocket-req-res"));
+            s.take(10).doOnNext(p -> System.out.println(p.getDataUtf8())).blockLast();
+        } finally {
+            clientRSocket.dispose();
+        }
     }
 }
