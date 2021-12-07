1fc77842879ca602637fb03024eed225cc9a6830
许久明 <xujiuming@mucang.cn>
2021-12-07 15:42:22 +0800

    feat:增加rsocket测试用例

--- rsocket/src/main/java/com/ming/rsocket/TestRsocketController.java
+++ rsocket/src/main/java/com/ming/rsocket/TestRsocketController.java
@@ -1,17 +1,38 @@
 package com.ming.rsocket;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @Controller
 @Slf4j
 public class TestRsocketController {
 
     @MessageMapping("test-rsocket-req-res")
-    String requestResponse(String message) {
+    public Mono<String> requestResponse(String message) {
         log.info("测试rsocket req-res模式:{}", message);
-        return "测试rsocket req-res模式," + message;
+        return Mono.just("测试rsocket req-res模式," + message);
     }
 
+    @MessageMapping("test-rsocket-req-stream")
+    public Flux<String> requestStream(String message) {
+        log.info("测试req-stream模式:{}", message);
+        return Flux.range(0, 100).map(m -> "测试req-stream模式" + m + ":" + message + ":" + LocalDateTime.now());
+    }
+
+    @MessageMapping("test-rsocket-fire-and-forget")
+    public Mono<Void> fireAndForget(String message) {
+        log.info("测试fire-and-forget模式:{}", message);
+        return Mono.empty();
+    }
+
+    @MessageExceptionHandler
+    public Mono<String> handlerException(Exception e) {
+        return Mono.just(e.getMessage());
+    }
 }
--- rsocket/src/test/java/com/ming/rsocket/TestRsocketClient.java
+++ rsocket/src/test/java/com/ming/rsocket/TestRsocketClient.java
@@ -7,7 +7,7 @@ public class TestRsocketClient {
     RSocketRequester requester = RSocketRequester.builder().tcp("localhost", 20000);
 
     @Test
-    public void testReqRes(){
+    public void testReqRes() {
         String str = requester.route("test-rsocket-req-res")
                 .data("fffffffffffff")
                 .retrieveMono(String.class)
@@ -15,4 +15,24 @@ public class TestRsocketClient {
         System.out.println(str);
     }
 
+    @Test
+    public void testReqStream() {
+        requester.route("test-rsocket-req-stream")
+                .data("fffffffffffffffffffffffffff")
+                .retrieveFlux(String.class)
+                .map(m -> {
+                    System.out.println(m);
+                    return m;
+                })
+                .blockLast();
+
+    }
+
+    @Test
+    public void testFireAndForget() {
+        requester.route("test-rsocket-fire-and-forget")
+                .data("fffffffffffffffffffffffffffffffffffff")
+                .send()
+                .block();
+    }
 }
